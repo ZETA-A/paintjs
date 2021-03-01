6940ba55ffd8e3ed7f26451ff3225253d788b606
A_ZETA_A <56914556+ZETA-A@users.noreply.github.com>
2021-03-01 21:33:58 +0900

    save

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+
 ctx.lineWidth = 2.5;
 
-canvas.width = 700;
-canvas.height = 700;
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPaintng() {
+    painting = true;
+}
 
 function onMouseMove(event) {
     const x = event.offsetX;
@@ -20,23 +37,31 @@ function onMouseMove(event) {
     }
 }
 
-let painting = false;
-
-function stopPainting() {
-    painting = false
-}
-
-function startPaintng() {
-    painting = true;
-}
-
-function onMouseLeave(event) {
-    painting = false;
-}
-
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 if (canvas){
@@ -44,6 +69,17 @@ if (canvas){
     canvas,addEventListener("mousedown", startPaintng);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+  );
+
+  if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick)
+}
