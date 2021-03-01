f42ea012e545c5b00fd810e14c32f7fb8642113d
A_ZETA_A <56914556+ZETA-A@users.noreply.github.com>
2021-03-01 20:41:44 +0900

    2.3 save

---
+++ app.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke()
+    }
+}
+
+let painting = false;
+
+function stopPainting() {
+    painting = false
+}
+
+function startPaintng() {
+    painting = true;
+}
+
+function onMouseLeave(event) {
+    painting = false;
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
+if (canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas,addEventListener("mousedown", startPaintng);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
